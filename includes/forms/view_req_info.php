ad9fa8fe4ca2414a1dda7089b65e05dc92cc0169
Kelvin Randa <kelvin.randa@untucapital.co.zw>
2024-08-20 10:04:44 +0200

    show PO users only on selecting approvers

--- includes/forms/view_req_info.php
+++ includes/forms/view_req_info.php
@@ -276,7 +276,7 @@
                             <select name="approvers[]" class="custom-select2 form-control" multiple="multiple" style="width: 100%">
                                 <optgroup label="Choose Approvers">
                                     <?php
-                                    $users = users();
+                                    $users = po_user();
                                     $req_approvers = requisitions('/'.$_GET['req_id']);
                                     foreach ($users as $user) {
                                         $userId = $user['id'];
@@ -496,7 +496,7 @@
                                                                 <?php
                                                                 $user = user($req['teller']);
                                                                 echo "<option value='$user[id]'>$user[firstName] $user[lastName]</option>";
-                                                                $users = untuStaff();
+                                                                $users = po_user();
                                                                 foreach ($users as $user) {
                                                                     echo "<option value='$user[id]'>$user[firstName] $user[lastName]</option>";
                                                                 }
